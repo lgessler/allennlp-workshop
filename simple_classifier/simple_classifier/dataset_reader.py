d21eb025596b7ec7734249ecc9056f77853a3b96
Luke Gessler <lukegessler@gmail.com>
2021-10-24 18:21:06 -0400

    update dataset reader

--- simple_classifier/simple_classifier/dataset_reader.py
+++ simple_classifier/simple_classifier/dataset_reader.py
@@ -1,10 +1,10 @@
 import json
 from typing import Dict, Iterable
 
-from allennlp.data import DatasetReader, Instance, Field
+from allennlp.data import DatasetReader, Instance
 from allennlp.data.fields import LabelField, TextField
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
-from allennlp.data.tokenizers import Tokenizer, WhitespaceTokenizer
+from allennlp.data.tokenizers import Tokenizer, SpacyTokenizer
 
 
 @DatasetReader.register("yelp-review-jsonl")
@@ -13,22 +13,35 @@ class YelpReviewJsonLinesReader(DatasetReader):
         self,
         tokenizer: Tokenizer = None,
         token_indexers: Dict[str, TokenIndexer] = None,
-        max_tokens: int = None,
         **kwargs
     ):
         super().__init__(**kwargs)
-        self.tokenizer = tokenizer or WhitespaceTokenizer()
+        self.tokenizer = tokenizer or SpacyTokenizer()
         self.token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
-        self.max_tokens = max_tokens
 
-    def text_to_instance(self, text: str, label: str = None) -> Instance:  # type: ignore
+    def text_to_instance(self, text: str, label: int) -> Instance:
+        """
+        Args:
+            text: a string value with the text of the Yelp review.
+            label: an int value from 0 to 4 (inclusive) which corresponds to the review's star rating
+
+        Returns:
+            An allennlp.data.Instance with the fields "text" and "label".
+            "text" is a TextField, and "label" is a "LabelField" where the original value of "label"
+            has been increased by one and turned into a string.
+        """
+        # The text of the yelp review.
+        # Note we need to replace the escaped newline characters and double quotes.
+        text = text.replace("\\n", "\n")
+        text = text.replace('\\"', '"')
+        # Use the supplied tokenizer to turn the text into tokens
         tokens = self.tokenizer.tokenize(text)
-        if self.max_tokens:
-            tokens = tokens[: self.max_tokens]
-        text_field = TextField(tokens, self.token_indexers)
-        fields: Dict[str, Field] = {"text": text_field}
-        if label:
-            fields["label"] = LabelField(label)
+        # The rating--these are numbers 0 through 4, make them strings "1" to "5"
+        label = str(1 + label)
+        fields = {
+            "text": TextField(tokens, self.token_indexers),
+            "label": LabelField(label),
+        }
         return Instance(fields)
 
     def _read(self, file_path: str) -> Iterable[Instance]:
@@ -36,11 +49,4 @@ class YelpReviewJsonLinesReader(DatasetReader):
             for line in lines:
                 if line != "":
                     data = json.loads(line)
-                    # The text of the yelp review.
-                    # Note we need to replace the escaped newline characters and double quotes.
-                    text = data["text"]
-                    text = text.replace("\\n", "\n")
-                    text = text.replace('\\"', '"')
-                    # The rating--these are numbers 0 through 4, make them strings "1" to "5"
-                    label = str(1 + data["label"])
-                    yield self.text_to_instance(text, label)
+                    yield self.text_to_instance(data["text"], data["label"])
